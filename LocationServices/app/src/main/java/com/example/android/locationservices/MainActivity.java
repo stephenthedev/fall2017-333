744629dcb3454fb63a50fcf8b16616ef0c365107
Stephen Raghunath <raghunat@fredonia.edu>
2017-11-08 14:44:15 -0500

    Updated

--- LocationServices/app/src/main/java/com/example/android/locationservices/MainActivity.java
+++ LocationServices/app/src/main/java/com/example/android/locationservices/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -16,12 +17,30 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if  (grantResults.length > 0) {
+            // they granted permission, so start listening
+            try {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, locationListener);
+            } catch (SecurityException e) {
+                Toast.makeText(this, "No permission granted, restart to try again", Toast.LENGTH_LONG).show();
+            }
+
+        } else {
+            Toast.makeText(this, "No permission granted, restart to try again", Toast.LENGTH_LONG).show();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv = (TextView)findViewById(R.id.textView);
+        final TextView tv = (TextView)findViewById(R.id.textView);
 
         // Set up a location manager: handles events of location
         // on update of location, update the text view
@@ -29,14 +48,15 @@ public class MainActivity extends AppCompatActivity {
         // Handle location permissions if not enabled
 
         // Get the existing location manager instance from the device by CONSTANT ID
-        LocationManager locationManger = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+        locationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
         final Context context = this;
 
         // Listener = the intent that has all the methods for dealing with location
-        LocationListener locationListener = new LocationListener() {
+        locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
                 Log.i("LOCATION", "Location changed" + location.toString());
+                tv.setText("Lat: " + location.getLatitude() + " Long: " + location.getLongitude());
             }
 
             @Override
@@ -60,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
         ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
                 this, Manifest.permission.INTERNET
         ) == PackageManager.PERMISSION_GRANTED) {
-            locationManger.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, locationListener);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, locationListener);
         } else {
             Toast.makeText(this, "Permission not granted, asking for permission", Toast.LENGTH_LONG).show();
 
@@ -74,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
                 2 // A custom integer we can define as the request code
                   // To identify the extra intent this creates (which is the ask for permission)
             );
-            locationManger.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 0, locationListener);
+            // locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, locationListener);
         }
 
     }
