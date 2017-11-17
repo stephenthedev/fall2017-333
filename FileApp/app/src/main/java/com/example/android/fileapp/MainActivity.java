b98ac6e90bd537f0d2aad9adf682b42a2903c573
Stephen Raghunath <straghunath@gmail.com>
2017-11-17 14:51:43 -0500

    Update MainActivity.java

--- FileApp/app/src/main/java/com/example/android/fileapp/MainActivity.java
+++ FileApp/app/src/main/java/com/example/android/fileapp/MainActivity.java
@@ -1,7 +1,8 @@
-package com.example.android.fileapp;
+package com.example.fileapp;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.os.Environment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,13 +11,18 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import static android.os.Environment.getExternalStorageDirectory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,17 +31,18 @@ public class MainActivity extends AppCompatActivity {
     EditText newContentEditText;
     EditText newTitleEditText;
     LinearLayout fileListLayout;
+    LinearLayout externalFileListLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        externalListTextView = (TextView)findViewById(R.id.externalList);
         isInternalButton = (ToggleButton)findViewById(R.id.internalToggle);
         newContentEditText = (EditText)findViewById(R.id.newContent);
         newTitleEditText = (EditText)findViewById(R.id.newTitle);
         fileListLayout = (LinearLayout)findViewById(R.id.fileList);
+        externalFileListLayout = (LinearLayout)findViewById(R.id.externalFileList);
 
         // Populate the text views on create
         updateListOfExternalFiles();
@@ -61,9 +68,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void createExternalFile(String fileName, String fileContent) {
-        // Check if external file directory is mounted
+        // Check if external file directory is mounted and writable
+        if (!Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            // no external drive detected
+            Toast.makeText(this, "Media not mounted", Toast.LENGTH_SHORT).show();
+            return; // stop here
+        }
+
         // Get the location of the external app storage
+        File externalDirectory = getExternalMediaDirs()[0];
+
         // Write the file there
+        // TODO write the File just like the other File Classess
+        File newFile = new File(externalDirectory, fileName);
+
+        try {
+            newFile.createNewFile();
+            FileOutputStream output = new FileOutputStream(newFile);
+            OutputStreamWriter writer = new OutputStreamWriter(output);
+
+            writer.append(fileContent);
+            writer.close();
+
+            output.flush();
+            output.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        // refresh the view
+        updateListOfExternalFiles();
     }
 
     public void updateListOfInternalFiles() {
@@ -124,7 +158,60 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void updateListOfExternalFiles() {
-        // Update the list of external files
+        // Update the list of internal files
+        // NOTE: "File" represents both files and folders in android
+        File externalDir = getExternalMediaDirs()[0];
+        String newList = "List is: \n";
+        File[] files = externalDir.listFiles();
+
+        final Context context = this;
+
+        externalFileListLayout.removeAllViews();
+
+        for (int i = 0; i < files.length; i ++) {
+            TextView fileTextView = new TextView(this);
+            fileTextView.setText(files[i].getName());
+            fileTextView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    File externalDir = getExternalMediaDirs()[0];
+                    final TextView tv = (TextView)v;
+                    // using the intenrnal path and the textview's text, get the file
+                    // reference
+                    File file = new File(externalDir, tv.getText().toString());
+
+                    StringBuilder text = new StringBuilder();
+
+                    try {
+                        BufferedReader br = new BufferedReader(new FileReader(file));
+
+                        String line;
+                        while ((line = br.readLine()) != null) {
+                            text.append(line + "\n");
+                        }
+
+                        br.close();
+                    } catch(IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    new AlertDialog.Builder(context)
+                            .setTitle(tv.getText())
+                            .setMessage(text.toString())
+                            .setPositiveButton("OK", null)
+                            .setNegativeButton("DELETE", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int option) {
+                                    deleteFile(tv.getText().toString());
+                                    updateListOfInternalFiles();
+                                }
+                            })
+                            .create()
+                            .show();
+                }
+            });
+            externalFileListLayout.addView(fileTextView);
+        }
     }
 
     public void deleteFile(String fileName, boolean isInternal) {
@@ -132,12 +219,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onCreateClick(View view) {
-        // TODO handle the toggle button
-        // TODO create a way to specify filename vs file content
-
-        createInternalFile(
-            newTitleEditText.getText().toString(),
-            newContentEditText.getText().toString()
-        );
+        if (isInternalButton.isChecked()) {
+            createInternalFile(
+                newTitleEditText.getText().toString(),
+                newContentEditText.getText().toString()
+            );
+        } else {
+            createExternalFile(
+                newTitleEditText.getText().toString(),
+                newContentEditText.getText().toString()
+            );
+        }
     }
 }
